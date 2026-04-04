8da3719fd5a1dad1a9cce0517afeffd167c77a26
tj1125 <jenny.wendy38@gmail.com>
2026-04-04 22:26:44 +0800

    original speed & change tolerance

--- tools/car_control/src/car_control_pkg/car_control_pkg/action_config.py
+++ tools/car_control/src/car_control_pkg/car_control_pkg/action_config.py
@@ -6,8 +6,8 @@ vel, rotate_vel為自走車PID數值, 可於arduino程式碼查看
 
 前左、前右、後左、後右
 """
-vel = 15.0  #10.0
-vel_slow = 5.0  #3.0
+vel = 10.0
+vel_slow = 3.0
 rotate_vel = 9.0
 rotate_vel_slow = 2.0
 rotate_vel_median = 4.0
