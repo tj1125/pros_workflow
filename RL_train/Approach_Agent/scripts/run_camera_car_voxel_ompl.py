feb6bfa7eba9063c591fcdff273ff27b24f5f274
tj1125 <jenny.wendy38@gmail.com>
2026-04-25 20:41:15 +0800

    Prune unrelated Approach_Agent files

--- RL_train/Approach_Agent/scripts/run_camera_car_voxel_ompl.py
+++
@@ -1,5 +0,0 @@
-from src.camera_car_voxel_ompl import cli_entrypoint
-
-
-if __name__ == "__main__":
-    cli_entrypoint()
--- RL_train/Approach_Agent/scripts/test_pybullet_smoke.py
+++
@@ -1,5 +0,0 @@
-from src.pybullet_smoke import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
